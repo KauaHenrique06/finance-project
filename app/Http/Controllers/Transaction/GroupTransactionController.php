17625a21f284b2661a911347866eb4b12758c49e
KauaHenrique06 <kauahcarmo@gmail.com>
2026-08-17 22:34:21 -0300

    <REFAC>[transaction-group]: rename show to indexTransactionByGroupId

--- app/Http/Controllers/Transaction/GroupTransactionController.php
+++ app/Http/Controllers/Transaction/GroupTransactionController.php
@@ -6,8 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Transaction\AssignInstanceToGroupRequest;
 use App\Http\Requests\Transaction\AssignUserToTransactionGroupRequest;
 use App\Http\Requests\Transaction\DeleteTransactionGroupRequest;
+use App\Http\Requests\Transaction\IndexGroupTransactionRequest;
 use App\Http\Requests\Transaction\IndexTransactionByGroupIdRequest;
 use App\Http\Requests\Transaction\StoreTransactionRequest;
+use App\Http\Requests\Transaction\UpdateTransactionGroupRequest;
+use App\Http\Resources\Transaction\GroupTransactionCollection;
+use App\Http\Resources\Transaction\GroupTransactionResource;
 use App\Http\Resources\Transaction\TransactionCollection;
 use App\Services\Transaction\GroupTransactionService;
 use App\Support\ApiResponse;
@@ -21,16 +25,31 @@ class GroupTransactionController extends Controller
     public function __construct(protected GroupTransactionService $groupTransactionService) {}
 
     /**
+     * Index a user transaction group
+     * 
+     * 
+     */
+    public function index(IndexGroupTransactionRequest $request)
+    {
+        $data = $this->groupTransactionService->index($request->validated());
+        return ApiResponse::success(
+            new GroupTransactionCollection($data),
+            'Transactions group was indexed with success!',
+            200
+        );
+    }
+
+    /**
      * Create a transaction group
      *
-     * Splits the total amount across the instalments and returns the
-     * transactions that were generated.
+     * Splits the total amount across the instalments and returns the group
+     * with the transactions that were generated.
      */
     public function store(StoreTransactionRequest $request): JsonResponse
     {
         $data = $this->groupTransactionService->store($request->validated());
         return ApiResponse::success(
-            new TransactionCollection($data),
+            new GroupTransactionResource($data),
             'Transaction group was created with success!',
             201
         );
@@ -39,9 +58,9 @@ class GroupTransactionController extends Controller
     /**
      * List the transactions of a group
      */
-    public function show(IndexTransactionByGroupIdRequest $request): JsonResponse
+    public function indexTransactionByGroupId(IndexTransactionByGroupIdRequest $request): JsonResponse
     {
-        $data = $this->groupTransactionService->show($request->validated());
+        $data = $this->groupTransactionService->indexTransactionByGroupId($request->validated());
         return ApiResponse::success(
             new TransactionCollection($data),
             'Transactions was indexed with success!',
@@ -60,6 +79,22 @@ class GroupTransactionController extends Controller
         return ApiResponse::success(
             null,
             'Transaction group was deleted with success!',
+            200
+        );
+    }
+
+    /**
+     * Update a transaction group
+     *
+     * Update group and your transactions when send on request. Returns the
+     * group with the resulting transactions.
+     */
+    public function update(UpdateTransactionGroupRequest $request): JsonResponse
+    {
+        $data = $this->groupTransactionService->update($request->validated());
+        return ApiResponse::success(
+            new GroupTransactionResource($data),
+            'Transaction group was updated with success!',
             200
         );
     }
--- routes/api/transaction_group.php
+++ routes/api/transaction_group.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\Transaction\GroupTransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth.api')->group(function() {
+    Route::get('/', [GroupTransactionController::class, 'index'])->middleware('can:transactionGroup.view');
     Route::post('/', [GroupTransactionController::class, 'store'])->middleware('can:transactionGroup.create');
-    Route::get('/{id}', [GroupTransactionController::class, 'show'])->middleware('can:transactionGroup.view');
+    Route::get('/{id}', [GroupTransactionController::class, 'indexTransactionByGroupId'])->middleware('can:transactionGroup.view');
+    Route::patch('/{id}', [GroupTransactionController::class, 'update'])->middleware('can:transactionGroup.update');
     Route::delete('/{id}', [GroupTransactionController::class, 'destroy'])->middleware('can:transactionGroup.delete');
     Route::post('/{id}/participant', [GroupTransactionController::class, 'assignParticipant'])->middleware('can:transactionGroup.assignParticipant');
     Route::patch('/{id}/instance', [GroupTransactionController::class, 'assignInstanceToGroup'])->middleware('can:transactionGroup.assignInstance');
